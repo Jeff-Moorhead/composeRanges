bdd4eb8ef99a14b2b3435f39b7629ddc213629cd
jeff-moorhead <jeff.moorhead1@gmail.com>
2023-08-15 10:52:57 -0400

    add problem statement and a few comments

--- compose.go
+++ compose.go
@@ -1,14 +1,27 @@
 package composeRanges
 
+/*
+Problem statement:
+
+Given a sorted integer array that does not contain any duplicates, return a summary of the number ranges it contains.
+
+Example:
+
+For nums = [-1, 0, 1, 2, 6, 7, 9], the output should be
+	solution(nums) = ["-1->2", "6->7", "9"].
+*/
+
 import (
 	"strconv"
 )
 
+// A convenient wrapper for ranges (much cleaner than a map or array)
 type Range struct {
 	Start string
 	End   string
 }
 
+// This function just glues the final results together
 func Solution(nums []int) []string {
 
 	ranges := Compose(nums, nil)
@@ -26,8 +39,10 @@ func Solution(nums []int) []string {
 	return results
 }
 
+// This is the actual recursive solution to the DP problem
 func Compose(nums []int, results []Range) []Range {
 
+	// TODO: how can we use memoization here? Is it necessary?
 	if results == nil {
 		results = make([]Range, 0)
 	}
@@ -42,6 +57,7 @@ func Compose(nums []int, results []Range) []Range {
 	for end > start {
 
 		// For consecutive numbers in an array, the last element minus the first element SHOULD equal the final array index (unproven)
+		// TODO: is there a way to prove rigorously that the above statement is true for all consecutive arrays?
 		if nums[end]-nums[start] == end {
 			final := Range{
 				Start: strconv.Itoa(nums[start]),
